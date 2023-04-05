e05d0d3498045fdde912736f596b62be5b39b181
Aleksey Arabchik <aleksey-090@mail.ru>
2023-04-06 00:30:52 +0300

    main logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import Header from "./components/Header/Header";
+import AppRouter from "./components/AppRouter/AppRouter";
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <AppRouter />
+    </BrowserRouter>
+  );
 }
 
 export default App;
