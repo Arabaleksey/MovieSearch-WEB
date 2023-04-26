013fc7c2a182eb301bc71bb4b794e2b864151459
Aleksey Arabchik <aleksey-090@mail.ru>
2023-04-26 21:34:35 +0300

    fix

--- src/http/interceptors.ts
+++ src/http/interceptors.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { AuthResponse } from "../interfaces/response/AuthResponse";
 import { LOCAL_STORAGE_KEYS } from "../constants/LocalStorageKeys";
 
-
-export const API_URL = `http://localhost:5000/api`;
+// export const API_URL = `http://localhost:5000/api`;
+export const API_URL = `https://node-movie-search-web.vercel.app/api`;
 
 const $api = axios.create({
   withCredentials: true,
@@ -11,7 +11,9 @@ const $api = axios.create({
 });
 
 $api.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN)}`;
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    LOCAL_STORAGE_KEYS.ACCESS_TOKEN
+  )}`;
   return config;
 });
 
@@ -21,13 +23,20 @@ $api.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+    if (
+      error.response.status == 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
       originalRequest._isRetry = true;
       try {
         const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {
           withCredentials: true,
         });
-        localStorage.setItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN, response.data.accessToken);
+        localStorage.setItem(
+          LOCAL_STORAGE_KEYS.ACCESS_TOKEN,
+          response.data.accessToken
+        );
         return $api.request(originalRequest);
       } catch (e) {
         alert("Не авторизован");
