3883ada501561ad30a5b4d61a977cfd6f8746317
Aleksey Arabchik <aleksey-090@mail.ru>
2023-05-02 21:29:25 +0300

    fix

--- src/http/interceptors.ts
+++ src/http/interceptors.ts
@@ -5,6 +5,7 @@ import { LOCAL_STORAGE_KEYS } from "../constants/LocalStorageKeys";
 // export const API_URL = `http://localhost:5000/api`;
 export const API_URL = `https://node-movie-search-web.vercel.app/api`;
 
+axios.defaults.withCredentials = true;
 const $api = axios.create(<any>{
   withCredentials: true,
   baseURL: API_URL,
@@ -30,7 +31,9 @@ $api.interceptors.response.use(
     ) {
       originalRequest._isRetry = true;
       try {
-        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, <any>{
+        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, <
+          any
+        >{
           headers: {
             Authorization: `Bearer ${localStorage.getItem(
               LOCAL_STORAGE_KEYS.ACCESS_TOKEN
